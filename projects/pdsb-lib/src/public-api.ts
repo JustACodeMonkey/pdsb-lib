59def78ae3691c3511918e070baf6f275e077208
Greg <greg.yantz@peelsb.com>
2020-08-20 16:36:37 -0400

    This was deleted somehow ...

---
+++ projects/pdsb-lib/src/public-api.ts
@@ -0,0 +1,4 @@
+export * from './lib/pdsb-lib.module';
+export * from './lib/storage-manager/storage-manager.service';
+export * from './lib/window-manager/message-data';
+export * from './lib/window-manager/window-manager.service';
